4399d0919c4c927a58cbe230f793d0f4a3aa5fa3
ASalem404 <70176109+ASalem404@users.noreply.github.com>
2022-12-16 15:05:10 +0200

    Add files via upload
    initiating the main tour controller function exported from handleOps controller

---
+++ server/controllers/tourController.js
@@ -0,0 +1,33 @@
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+const {
+    getAll,
+    getOne,
+    createOne,
+    updateOne,
+    deleteOne,
+} = require('./handleOps');
+
+exports.getAllTours = getAll(Tour);
+exports.getTour = getOne(Tour);
+exports.createTour = createOne(Tour);
+
+exports.updateTour = updateOne(Tour);
+exports.deleteTour = deleteOne(Tour);
+
+exports.search = catchAsync(async(req, res, next) => {
+    const filter = req.body;
+    data = await Tour.find(filter);
+    if (data.length === 0) {
+        return res.status(200).json({
+            status: 'success',
+            message: 'No such tours founded in this time',
+            data,
+        });
+    }
+    res.status(200).json({
+        status: 'success',
+        result: data.length,
+        data,
+    });
+});
