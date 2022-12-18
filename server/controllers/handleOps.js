49c32292f287ecf163b1bfc2bcc6915eaeb0be0c
MostafaMGomaa <gomaamostafa26@gmail.com>
2022-12-18 10:53:40 +0200

    fininsh tourModel , handleOps

--- server/controllers/handleOps.js
+++ server/controllers/handleOps.js
@@ -3,73 +3,73 @@ const APIFeatures = require('../utils/apiFeatures');
 const AppError = require('../utils/appError');
 
 exports.getAll = (Model) =>
-    catchAsync(async(req, res, next) => {
-        let filter = {};
-        if (req.params.tourId) filter = { tour: req.params.tourId };
-        const features = new APIFeatures(Model.find(filter), req.query)
-            .filter()
-            .sort()
-            .limit()
-            .pagnation();
-        const data = await features.query;
-        // SEND RESPONSE
-        res.status(200).json({
-            status: 'success',
-            result: data.length,
-            data,
-        });
+  catchAsync(async (req, res, next) => {
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limit()
+      .pagnation();
+    const data = await features.query;
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      result: data.length,
+      data,
     });
+  });
 
 exports.getOne = (Model, popOptions) =>
-    catchAsync(async(req, res, next) => {
-        let query = await Model.findById(req.params.id);
-        const data = await query;
-        if (!data) {
-            return next(new AppError(`Cannot find any result with this ID`, 404));
-        }
-        // SEND RESPONSE
-        res.status(200).json({
-            status: 'success',
-            data,
-        });
+  catchAsync(async (req, res, next) => {
+    let query = await Model.findById(req.params.id).popual;
+    const data = await query.populate(popOptions);
+    if (!data) {
+      return next(new AppError(`Cannot find any result with this ID`, 404));
+    }
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      data,
     });
+  });
 
 exports.createOne = (Model) =>
-    catchAsync(async(req, res, next) => {
-        const data = await Model.create(req.body);
+  catchAsync(async (req, res, next) => {
+    const data = await Model.create(req.body);
 
-        // SEND RESPONSE
-        res.status(201).json({
-            status: 'success',
-            data,
-        });
+    // SEND RESPONSE
+    res.status(201).json({
+      status: 'success',
+      data,
     });
+  });
 
 exports.updateOne = (Model) =>
-    catchAsync(async(req, res, next) => {
-        const data = await Model.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-            runValidators: true,
-        });
-
-        if (!data)
-            return next(new AppError(404, `Cannot find any result with this ID`));
-
-        // SEND RESPONSE
-        res.status(200).json({
-            status: 'success',
-            data,
-        });
+  catchAsync(async (req, res, next) => {
+    const data = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
     });
+
+    if (!data)
+      return next(new AppError(404, `Cannot find any result with this ID`));
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      data,
+    });
+  });
 
 exports.deleteOne = (Model) =>
-    catchAsync(async(req, res, next) => {
-        const data = await Model.findByIdAndDelete(req.params.id);
+  catchAsync(async (req, res, next) => {
+    const data = await Model.findByIdAndDelete(req.params.id);
 
-        if (!data)
-            return next(new AppError(404, `Cannot find any result with this ID`));
-        res.status(204).json({
-            status: 'success',
-            message: 'tour deleted successfully',
-        });
+    if (!data)
+      return next(new AppError(404, `Cannot find any result with this ID`));
+    res.status(204).json({
+      status: 'success',
+      message: 'tour deleted successfully',
     });
+  });
--- server/models/tourModel.js
+++ server/models/tourModel.js
@@ -1,79 +1,79 @@
 const mongoose = require('mongoose');
 const tourSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'Tour must have a name'],
-        unique: [true, 'Tour name must be unique'],
-        maxlength: [70, 'the name must be less or equal then 40 characters'],
-        minlength: [10, 'the name must be more or equal then 10 characters'],
+  name: {
+    type: String,
+  },
+  fromCountry: {
+    type: String,
+    required: [true, 'the tour must have from country property'],
+  },
+  toCountry: {
+    type: String,
+    required: [true, 'the tour must have destination country '],
+  },
+  fromCity: {
+    type: String,
+    required: [true, 'the tour must have from City property'],
+  },
+  toCity: {
+    type: String,
+    required: [true, 'the tour must have destination City '],
+  },
+  fromSite: {
+    type: String,
+    required: [true, 'the tour must have Airport/Station'],
+  },
+  toSite: {
+    type: String,
+    required: [true, 'the tour must have Airport/Station'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  summary: {
+    type: String,
+    require: [true, 'must add tour summary'],
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'the tour must have price'],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+    min: [1, 'Rating must be above 1.0'],
+    max: [5, 'Rating must be below 5.0'],
+    set: (val) => Math.round(val * 10) / 10, // 4.666666, 46.6666, 47, 4.7
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  startDates: [Date],
+  duration: {
+    type: Number,
+    required: [true, 'must add tour duration time'],
+  },
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function (val) {
+        return this.price > val;
+      },
+      message: 'discount price must be less than price',
     },
-    fromCountry: {
-        type: String,
-        required: [true, 'the tour must have from country property'],
+  },
+  transportation: {
+    type: String,
+    trim: true,
+    enum: {
+      values: ['Plan', 'Bus', 'Car'],
+      message: 'invalid input, must be one of [Plan, Bus, Car]',
     },
-    toCountry: {
-        type: String,
-        required: [true, 'the tour must have destination country '],
-    },
-    fromCity: {
-        type: String,
-        required: [true, 'the tour must have from City property'],
-    },
-    toCity: {
-        type: String,
-        required: [true, 'the tour must have destination City '],
-    },
-    fromSite: {
-        type: String,
-        required: [true, 'the tour must have Airport/Station'],
-    },
-    toSite: {
-        type: String,
-        required: [true, 'the tour must have Airport/Station'],
-    },
-    description: {
-        type: String,
-        require: [true, 'must add tour description'],
-        trim: true,
-    },
-    summary: {
-        type: String,
-        require: [true, 'must add tour summary'],
-        trim: true,
-    },
-    price: {
-        type: Number,
-        required: [true, 'the tour must have price'],
-    },
-    startDates: [Date],
-    duration: {
-        type: Number,
-        required: [true, 'must add tour duration time'],
-    },
-    rating: {
-        type: Number,
-        default: 4.5,
-        max: [5, 'must be less or equal to 5'],
-        min: [1, 'must be more or equal to 1'],
-    },
-    priceDiscount: {
-        type: Number,
-        validate: {
-            validator: function(val) {
-                return this.price > val;
-            },
-            message: 'discount price must be less than price',
-        },
-    },
-    transportation: {
-        type: String,
-        trim: true,
-        enum: {
-            values: ['Plan', 'Bus', 'Car'],
-            message: 'invalid input, must be one of [Plan, Bus, Car]',
-        },
-        default: 'Plan',
-    },
+    default: 'Plan',
+  },
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
