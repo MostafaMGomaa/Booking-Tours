d6aeca02b5326c14a09ccb8cfd1aab817bcc9bcd
MostafaMGomaa <gomaamostafa26@gmail.com>
2023-03-31 02:50:00 +0200

    update file tree

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
+console.log(__dirname);
 const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
