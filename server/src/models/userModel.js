f3216d16abac6be091d65a9575998960e2ba66d4
MostafaMGomaa <gomaamostafa26@gmail.com>
2023-05-27 15:57:25 +0300

    trim and lowercase username in updateUserData api

--- server/src/models/userModel.js
+++ server/src/models/userModel.js
@@ -8,6 +8,8 @@ const userSchema = new mongoose.Schema(
     name: {
       type: String,
       required: [true, 'Please tell us your name!'],
+      trim: true,
+      lowercase: true,
     },
     email: {
       type: String,
