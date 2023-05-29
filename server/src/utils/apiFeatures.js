83cb44e40d989e2010145cb4d1927b0ded8ba2d2
MostafaMGomaa <gomaamostafa26@gmail.com>
2023-05-29 20:03:20 +0300

    update sort

--- server/src/utils/apiFeatures.js
+++ server/src/utils/apiFeatures.js
@@ -27,7 +27,7 @@ class APIFeatures {
       const sortBy = this.queryString.sort.split(',').join(' ');
       this.query = this.query.sort(sortBy);
     } else {
-      this.query = this.query.sort('-takeOff');
+      this.query = this.query.sort('takeOff');
     }
     return this;
   }
